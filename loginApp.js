5bdc373de8b202bd842d0b5d6531e5ed003c499a
Ivan Quesada <ivan191956@hotmail.com>
2016-06-01 09:46:46 -0600

    se agrega funcion que crea carpeta de usuario al ingresar

--- loginApp.js
+++ loginApp.js
@@ -18,18 +18,47 @@ app.controller("loginCtrl", ['$scope','$http','$window', function($scope, $http,
 		var req = {
 			method: 'POST',
 			url: 'http://sharelatex.cenat.ac.cr:8080/interfazWRF/Login',
+			headers: {
+				'Content-Type': 'text/javascript'
+			},
 			data: vm.username+"&"+vm.password
-		};
+		}
 
-		$http(req).then(function (response) {       //successCallback
+		$http(req).then(function (response) {       //successCallback, status, headers, config
 			console.log("entre al exitoso");//debugger borrar--------------
-			console.log(response.data);//debugger borrar--------------
-    			$scope.result = response.data;
-    		// $window.location.href = "" ---aqui va el link de la pagina que sigue
-  		}, function (response) {
-    	    		console.log("entre al error");//debugger borrar--------------
-    	   		console.log(response.data);//debugger borrar--------------
-    		//$scope.error = response.data;
+			console.log(response);//debugger borrar--------------
+			var respuesta = response.data;
+			var comparacion = "autenticado";
+			console.log(typeof respuesta);//debugger borrar--------------
+			console.log(typeof comparacion);//debugger borrar--------------
+			//compara lo que devuelve el servicio
+			if(respuesta.localeCompare(comparacion)){
+				console.log("entre en el autenticado t envio a proyectos");//debugger borrar--------------
+				//funcion que envia los datos para el nuevo proyecto 
+				var req2 = {
+							method: 'POST', 
+							url: 'http://sharelatex.cenat.ac.cr:8080/interfazWRF/Proyectos',
+							data: "nuser"+"¡"+vm.username
+				};
+				$http(req2).then(function (response) {       //successCallback
+						console.log("entre al exitoso");//debugger borrar--------------
+						console.log(response.data);//debugger borrar--------------
+  				}, function (response) {
+    	    			console.log("entre al error");//debugger borrar--------------
+						console.log(response.data);//debugger borrar--------------
+  				});
+  				//--------------------------------------------------	
+				$window.location.href = "proyectos.html";
+			}else if(respuesta.localeCompare("error")){
+				console.log("entre al error tengo q mostrar uno ");//debugger borrar--------------
+				document.getElementById("error").innerHTML="Usuario o Contraseña incorrectos";
+			}
+    		
+  		}, function (data) {
+    	    	console.log("entre al error");//debugger borrar--------------
+    	   		console.log(data);//debugger borrar--------------
+    	   		document.getElementById("error").innerHTML="Usuario o Contraseña incorrectos";
+    	   		
   		});
 			
 	};
